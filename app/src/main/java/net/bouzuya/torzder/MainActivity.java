2603ce30df824607a80f6ac859f17acfea16cb2d
bouzuya <m@bouzuya.net>
2015-04-11 16:04:23 +0900

    add ListButton

--- app/src/main/java/net/bouzuya/torzder/MainActivity.java
+++ app/src/main/java/net/bouzuya/torzder/MainActivity.java
@@ -1,9 +1,11 @@
 package net.bouzuya.torzder;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +14,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
