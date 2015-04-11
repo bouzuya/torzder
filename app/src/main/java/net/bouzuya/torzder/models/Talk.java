3202736c7a1340d2bf2c795784f8abe0a76aff4a
bouzuya <m@bouzuya.net>
2015-04-11 19:00:01 +0900

    add Talk#toString

--- app/src/main/java/net/bouzuya/torzder/models/Talk.java
+++ app/src/main/java/net/bouzuya/torzder/models/Talk.java
@@ -4,4 +4,13 @@ public class Talk {
     public int id;
     public String title;
     public String speaker;
+
+    @Override
+    public String toString() {
+        return "Talk{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", speaker='" + speaker + '\'' +
+                '}';
+    }
 }
