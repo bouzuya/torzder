323d01626cbc568fdf14a0c33c93ec60d19ab735
bouzuya <m@bouzuya.net>
2015-04-12 22:53:58 +0900

    add Caption model

---
+++ app/src/main/java/net/bouzuya/torzder/models/Caption.java
@@ -0,0 +1,8 @@
+package net.bouzuya.torzder.models;
+
+public class Caption {
+    public int startTime;
+    public boolean startOfParagraph;
+    public String content;
+    public int duration;
+}
