b1beb52a1e118195f0483e782c326828154ddef4
bouzuya <m@bouzuya.net>
2015-04-13 19:50:47 +0900

    add Caption#getStartTimeString

--- app/src/main/java/net/bouzuya/torzder/models/Caption.java
+++ app/src/main/java/net/bouzuya/torzder/models/Caption.java
@@ -5,4 +5,10 @@ public class Caption {
     public boolean startOfParagraph;
     public String content;
     public int duration;
+
+    public String getStartTimeString() {
+        int minutes = startTime / 1000 / 60; // minutes
+        int seconds = startTime / 1000 % 60; // seconds
+        return String.format("%02d:%02d", minutes, seconds);
+    }
 }
